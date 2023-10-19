1c749544a90510fd54e38c133cb0a58f450961ba
IdaHolmen <143997448+IdaHolmen@users.noreply.github.com>
2023-10-19 10:33:35 +0200

    Subtract button works!

--- js/script.js
+++ js/script.js
@@ -20,4 +20,13 @@ addButton.addEventListener("click", ()=>{
 	}
 });
 
-// subtractButton.addEventListener("click", () =>)
+subtractButton.addEventListener("click", () => {
+	const value1 = parseFloat(number1.value);
+	const value2 = parseFloat(number2.value);
+
+	if (!isNaN(value1) && !isNaN(value2)) {
+		sumElement.textContent = value1 - value2;
+	} else {
+		sumElement.textContent = "Invalid output";
+	}
+});
